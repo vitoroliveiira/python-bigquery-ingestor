6ea946c88f90888fecdc9cb6448ad1336501d757
Vitor Oliveira <vitorgabrieldeoliveira01@gmail.com>
2026-05-06 01:03:57 -0300

    dockerfile

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from google.cloud import bigquery
+import json
+
+app = FastAPI()
+
+client = bigquery.Client()
+
+TABLE_ID = "vitor-oliveira-analytics.tracking.events_raw"
+
+
+@app.get("/")
+def health():
+    return {"status": "ok"}
+
+
+@app.post("/event")
+async def event(request: Request):
+    data = await request.json()
+
+    row = {
+        "event_name": data.get("event_name"),
+        "user_id": data.get("user_id"),
+        "timestamp": data.get("timestamp"),
+        "payload": json.dumps(data)
+    }
+
+    errors = client.insert_rows_json(TABLE_ID, [row])
+
+    if errors:
+        return {"status": "error", "details": errors}
+
+    return {"status": "ok"}

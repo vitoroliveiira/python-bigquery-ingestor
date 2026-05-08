afacc6e2abb2e398b82061c9503089e7cc7d6ec5
Vitor Oliveira <vitorgabrieldeoliveira01@gmail.com>
2026-05-08 18:55:12 -0300

    timestamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, HTTPException
 from google.cloud import bigquery
+from datetime import datetime, UTC
 import hashlib
 import hmac
 import json
@@ -34,24 +35,42 @@ async def event(request: Request):
     # 2. lê JSON do evento
     data = await request.json()
 
-    # 3. reconstrói exatamente a string usada no GTM
+    # 3. pega timestamp original em ms
+    timestamp_ms = (
+        data.get("x-sst-system_properties", {})
+        .get("request_start_time_ms")
+    )
+
+    # 4. converte timestamp para formato aceito pelo BigQuery
+    timestamp = None
+
+    if timestamp_ms:
+        timestamp = datetime.fromtimestamp(
+            int(timestamp_ms) / 1000,
+            UTC
+        ).isoformat()
+
+    # 5. reconstrói exatamente a string usada no GTM
     raw_string = (
         f"{data.get('event_name')}|"
         f"{data.get('client_id')}|"
+        f"{timestamp_ms}|"
         f"{SECRET_KEY}"
     )
 
-    # 4. gera hash SHA256
+    # 6. gera hash SHA256
     generated_signature = hashlib.sha256(
         raw_string.encode("utf-8")
     ).hexdigest()
 
     # logs
     print("RAW STRING:", raw_string)
+    print("TIMESTAMP_MS:", timestamp_ms)
+    print("TIMESTAMP:", timestamp)
     print("RECEIVED SIGNATURE:", received_signature)
     print("GENERATED SIGNATURE:", generated_signature)
 
-    # 5. valida assinatura
+    # 7. valida assinatura
     if not hmac.compare_digest(
         received_signature,
         generated_signature
@@ -61,16 +80,19 @@ async def event(request: Request):
             detail="Invalid signature"
         )
 
-    # 6. insere no BigQuery
+    # 8. insere no BigQuery
     row = {
         "event_name": data.get("event_name"),
         "user_id": data.get("client_id"),
+        "timestamp": timestamp,
         "payload": json.dumps(data)
     }
 
     errors = client.insert_rows_json(TABLE_ID, [row])
 
     if errors:
+        print(errors)
+
         return {
             "status": "error",
             "details": errors
